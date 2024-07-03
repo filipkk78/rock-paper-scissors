0430e0e5a23d89936dfa1d460559ce519e8ec541
filipkk78 <filipkk78@gmail.com>
2024-07-03 13:38:00 +0200

    Add playRound function

--- game.js
+++ game.js
@@ -22,17 +22,56 @@ let humanChoice;
 let humanChoiceInput;
 function getHumanChoice() {
     do {
-    humanChoiceInput = prompt("Enter your choice: ");
-    if (humanChoiceInput == "Scissors"||humanChoiceInput == "scissors"||humanChoiceInput == "SCISSORS") {
+    humanChoiceInput = prompt("Enter your choice: ").toLowerCase();
+    if (humanChoiceInput == "scissors") {
         humanChoice = "scissors"
-    } else if (humanChoiceInput == "Rock"||humanChoiceInput == "rock"||humanChoiceInput == "ROCK") {
+    } else if (humanChoiceInput == "rock") {
         humanChoice = "rock"
-    } else if (humanChoiceInput == "Paper"||humanChoiceInput == "paper"||humanChoiceInput == "PAPER") {
+    } else if (humanChoiceInput == "paper"){
         humanChoice = "paper"
-    } else {
-        console.log("Please enter rock, paper or scissors.")
-    }}
+    } }
     while(humanChoice!="rock"&&humanChoice!="paper"&&humanChoice!="scissors")
 }//Asks the player their choice and assigns it to a variable
+let humanScore = 0
+let computerScore = 0
+function playRound() {
+        if(humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You chose rock. Computer chose scissors. You win.")
+        humanScore++
+        console.log(`You have ${humanScore} points. Computer has ${computerScore}.`)
+    }   else if(humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You chose paper. Computer chose rock. You win.")
+        humanScore++
+        console.log(`You have ${humanScore} points. Computer has ${computerScore}.`)
+    }   else if(humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You chose scissors. Computer chose paper. You win.")
+        humanScore++
+        console.log(`You have ${humanScore} points. Computer has ${computerScore}.`)
+    }   //Possibilities where player wins
+        else if(humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You chose rock. Computer chose paper. Computer wins.")
+        computerScore++
+        console.log(`You have ${humanScore} points. Computer has ${computerScore}.`)
+    }   else if(humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You chose paper. Computer chose scissors. Computer wins.")
+        computerScore++
+        console.log(`You have ${humanScore} points. Computer has ${computerScore}.`)
+    }   else if(humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You chose scissors. Computer chose rock. Computer wins.")
+        computerScore++
+        console.log(`You have ${humanScore} points. Computer has ${computerScore}.`)
+    }   //Possibilities where computer wins
+        else if(humanChoice == "rock" && computerChoice == "rock") {
+        console.log("You chose rock. Computer chose rock. It's a tie.")
+        console.log(`You have ${humanScore} points. Computer has ${computerScore}.`)
+    }   else if(humanChoice == "paper" && computerChoice == "paper") {
+        console.log("You chose paper. Computer chose paper. It's a tie.")
+        console.log(`You have ${humanScore} points. Computer has ${computerScore}.`)
+    }   else if(humanChoice == "scissors" && computerChoice == "scissors") {
+        console.log("You chose scissors. Computer chose scissors. It's a tie.")
+        console.log(`You have ${humanScore} points. Computer has ${computerScore}.`)
+    }   //Possibilities where it's a tie
+}
 getHumanChoice();
-console.log(humanChoice);
+getComputerChoice();
+playRound();
