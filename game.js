fe48b39b4a3d02b294890fb9dfcecb07ed8eb2f0
filipkk78 <filipkk78@gmail.com>
2024-07-03 04:24:42 +0200

    Add player choice function

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-let randomNumber
+let randomNumber;
 function getRandomNumber(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
@@ -10,12 +10,29 @@ let computerChoice;
 function getComputerChoice() {
     getRandomNumber(1, 3);
     if (randomNumber === 1) {
-        computerChoice = "Rock"
+        computerChoice = "rock"
     } else if (randomNumber === 2) {
-        computerChoice = "Paper"
+        computerChoice = "paper"
     } else if (randomNumber === 3) {
-        computerChoice = "Scissors"
+        computerChoice = "scissors"
     }
 }//Gets computer's choice and assigns the corresponding string
-getComputerChoice();
-console.log(computerChoice);
+
+let humanChoice;
+let humanChoiceInput;
+function getHumanChoice() {
+    do {
+    humanChoiceInput = prompt("Enter your choice: ");
+    if (humanChoiceInput == "Scissors"||humanChoiceInput == "scissors"||humanChoiceInput == "SCISSORS") {
+        humanChoice = "scissors"
+    } else if (humanChoiceInput == "Rock"||humanChoiceInput == "rock"||humanChoiceInput == "ROCK") {
+        humanChoice = "rock"
+    } else if (humanChoiceInput == "Paper"||humanChoiceInput == "paper"||humanChoiceInput == "PAPER") {
+        humanChoice = "paper"
+    } else {
+        console.log("Please enter rock, paper or scissors.")
+    }}
+    while(humanChoice!="rock"&&humanChoice!="paper"&&humanChoice!="scissors")
+}//Asks the player their choice and assigns it to a variable
+getHumanChoice();
+console.log(humanChoice);
