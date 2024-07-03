c8a3db2a519a41c7e205c1dad9fb663f1aed638b
filipkk78 <filipkk78@gmail.com>
2024-07-03 03:09:46 +0200

    Add computer choice selection

--- game.js
+++ game.js
@@ -1,15 +1,21 @@
 let randomNumber
-let computerChoice  
 function getRandomNumber(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     randomNumber = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); 
     return randomNumber;
-  }
-  
-  getRandomNumber(1,3);
-  console.log(randomNumber);
+}//Gets random number including both minimal and maximum number 
+
+let computerChoice;
 function getComputerChoice() {
     getRandomNumber(1, 3);
-    
-}
+    if (randomNumber === 1) {
+        computerChoice = "Rock"
+    } else if (randomNumber === 2) {
+        computerChoice = "Paper"
+    } else if (randomNumber === 3) {
+        computerChoice = "Scissors"
+    }
+}//Gets computer's choice and assigns the corresponding string
+getComputerChoice();
+console.log(computerChoice);
