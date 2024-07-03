50203513148fb01c9d090b0861aa60f0b96cc84a
filipkk78 <filipkk78@gmail.com>
2024-07-03 03:01:29 +0200

    Add random number generation

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+let randomNumber
+let computerChoice  
+function getRandomNumber(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    randomNumber = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); 
+    return randomNumber;
+  }
+  
+  getRandomNumber(1,3);
+  console.log(randomNumber);
+function getComputerChoice() {
+    getRandomNumber(1, 3);
+    
+}
